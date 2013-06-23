015a98119e6ceba954f8aff49e098f60ada149e8
Spencer Herzberg <spencer.herzberg@gmail.com>
2013-06-23 17:09:06 -0500

    adding more deps to puppet install

--- puppet/manifests/django.pp
+++ puppet/manifests/django.pp
@@ -4,8 +4,13 @@ exec {'apt_update':
   command => '/usr/bin/apt-get update',
 }
 
-package {'django-deps':
+package {'python-deps':
   name => ['python-pip'],
   ensure => present,
 }
 
+package {'django-deps':
+  name => ['django', 'django-debug-toolbar'],
+  ensure => present,
+  require => Package['python-deps'],
+}
