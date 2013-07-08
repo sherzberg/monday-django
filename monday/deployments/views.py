1788ec1fade11d3ec2523e63b31abffa0a40bad4
Spencer Herzberg <spencer.herzberg@gmail.com>
2013-07-08 17:13:36 -0500

    adding class based view

--- monday/deployments/views.py
+++ monday/deployments/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.views.generic import TemplateView
+from .models import Code
+
+class DeploymentView(TemplateView):
+    template_name = 'deployments/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DeploymentView, self).get_context_data(**kwargs)
+        context['object_list'] = Code.objects.all()
+        return context
--- monday/monday/urls.py
+++ monday/monday/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from deployments.api import CodeViewSet, DomainViewSet, ApplicationViewSet
 from rest_framework import routers
+from deployments.views import DeploymentView
 
-router = routers.DefaultRouter()
-router.register(r'code', CodeViewSet)
-router.register(r'domains', DomainViewSet)
-router.register(r'applications', ApplicationViewSet)
+
+#router = routers.DefaultRouter()
+#router.register(r'code', CodeViewSet)
+#router.register(r'domains', DomainViewSet)
+#router.register(r'applications', ApplicationViewSet)
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,7 +17,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'monday.views.home', name='home'),
     # url(r'^monday/', include('monday.foo.urls')),
-
+    url(r'deployments/$', DeploymentView.as_view()),
     #url(r'^api/', include(router.urls)),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     # Uncomment the next line to enable the admin:
